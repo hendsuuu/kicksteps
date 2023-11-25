49aade9a5255ea2651b5551d96409545f26aa1ad
hendsuuu <sutrishendra07@gmail.com>
2023-11-26 02:21:23 +0700

    frontend

---
+++ components/Shoes.jsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+
+const Shoes = () => {
+  return (
+    <section>
+      <div className="w-full grid gap-14 items-center grid-cols-4 place-content-center">
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (1).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (2).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (3).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (4).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (5).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (6).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (7).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (8).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (9).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (10).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (11).png"}
+          width={300}
+          height={225}
+        />
+        <Image
+          className="mx-auto"
+          alt={"sepatu"}
+          src={"/assets/1 (12).png"}
+          width={300}
+          height={225}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Shoes;
